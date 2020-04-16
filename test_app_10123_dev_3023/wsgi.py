63935a52e2a6ea899180b282bf1509bd527a5e16
Crowdbotics <team@crowdbotics.com>
2020-04-16 14:56:35 +0000

    Update models

--- test_app_10123_dev_3023/wsgi.py
+++ test_app_10123_dev_3023/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_app_10123_dev_3023.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_app_10123_dev_3023.settings")
 
 application = get_wsgi_application()
